3f645afa60eb0e660ac709c51da52cd6da653f4c
Zois Pagoulatos <zpagoulatos@hotmail.com>
2020-11-22 13:32:25 +0100

    Allow configuring debug log level for logrus using env var ROMIE_DEBUG

--- internal/utils/init.go
+++ internal/utils/init.go
@@ -1,6 +1,19 @@
 package utils
 
-import "github.com/spf13/afero"
+import (
+	"os"
+
+	log "github.com/sirupsen/logrus"
+	"github.com/spf13/afero"
+)
 
 // AppFS Application filesystem.
 var AppFS = &afero.Afero{Fs: afero.NewOsFs()}
+
+func init() {
+	// Set debug mode on
+	if os.Getenv("ROMIE_DEBUG") == "on" {
+		// TODO: This is temporary. Remove it once Cobra/Viper is configured, to handle it as a boolean flags
+		log.SetLevel(log.DebugLevel)
+	}
+}
