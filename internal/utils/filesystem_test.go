cddb83b2990e3e6c7b763821b39038016082a709
Panos Georgiadis <drpaneas@gmail.com>
2021-02-07 15:00:02 +0100

    Add tests

--- internal/utils/filesystem_test.go
+++ internal/utils/filesystem_test.go
@@ -5,6 +5,8 @@ import (
 	"log"
 	"os"
 	"path/filepath"
+	"runtime"
+	"strings"
 	"testing"
 
 	"github.com/libopenstorage/openstorage/pkg/chattr"
@@ -196,5 +198,80 @@ func TestRemove(t *testing.T) {
 				t.Errorf("Remove() error = %v, wantErr %v", err, tt.wantErr)
 			}
 		})
+	}
+}
+
+func ExampleCreateFile() {
+	err := CreateFile(existingFile)
+	if err != nil {
+		fmt.Println("Failed to create file because it already exists")
+	} else {
+		fmt.Println("File has been created successfully!")
+	}
+	// Output: Failed to create file because it already exists
+}
+
+func TestCreateFile(t *testing.T) {
+	if err := os.Mkdir(nonWritableDir, 0400); err != nil {
+		log.Fatalf("Cannot create non writable directory %q", nonWritableDir)
+	}
+
+	defer func() {
+		_ = os.Remove(nonExistingFile)
+		_ = os.RemoveAll(nonWritableDir)
+		_ = os.RemoveAll(nonExistingFolder)
+	}()
+
+	tests := []struct {
+		name     string
+		filepath string
+		wantErr  bool
+	}{
+		{
+			"Returns nil if file is created",
+			nonExistingFile,
+			false,
+		},
+		{
+			"Returns err if file is exists",
+			existingFile,
+			true,
+		},
+		{
+			"Returns nil if file is created along with parent dirs",
+			filepath.Join(nonExistingFolder, "file.txt"),
+			false,
+		},
+		{
+			"Returns err if cannot write the file",
+			filepath.Join(nonWritableDir, "should-not-write-this.txt"),
+			true,
+		},
+		{
+			"Returns err when provided filepath is empty",
+			"",
+			true,
+		},
+		{
+			"Returns err if parent directory couldn't be created",
+			filepath.Join(nonWritableDir, "parent/newdir/file.txt"),
+			true,
+		},
+	}
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			skipWindowsNonWritableDirScenario(t, tt.filepath, tt.name)
+
+			if err := CreateFile(tt.filepath); (err != nil) != tt.wantErr {
+				t.Errorf("CreateFile() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
+
+func skipWindowsNonWritableDirScenario(t *testing.T, file string, scenarioName string) {
+	if strings.Contains(file, filepath.Base(nonWritableDir)) && runtime.GOOS == "windows" {
+		t.Skipf("Skip %q test in windows", scenarioName)
 	}
 }
