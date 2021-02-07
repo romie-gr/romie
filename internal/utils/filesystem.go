253fdd8cf73ba45adbf05d8b380c0bbe1d3c2056
Panos Georgiadis <drpaneas@gmail.com>
2021-02-07 14:30:40 +0100

    Add createfile function

--- internal/utils/filesystem.go
+++ internal/utils/filesystem.go
@@ -1,7 +1,10 @@
 package utils
 
 import (
+	"fmt"
 	"os"
+
+	osfile "path/filepath"
 
 	log "github.com/sirupsen/logrus"
 )
@@ -44,4 +47,29 @@ func Remove(path string) error {
 	}
 
 	return os.Remove(path)
+}
+
+// CreateFile creates a file on the given filepath
+// along with any necessary parents, and returns nil,
+// or else returns an error.
+func CreateFile(filepath string) error {
+	// Avoid file truncate and return error instead
+	if FileExists(filepath) {
+		return fmt.Errorf("file %s already exists", filepath)
+	}
+
+	// Create the parent directory if doesn't exist
+	if directory := osfile.Dir(filepath); !FolderExists(directory) {
+		if err := os.MkdirAll(directory, os.ModePerm); err != nil {
+			return fmt.Errorf("failed to create directory %v", directory)
+		}
+	}
+
+	file, err := os.Create(filepath)
+	if err != nil {
+		return fmt.Errorf("failed to create file %v: %w", filepath, err)
+	}
+	defer file.Close()
+
+	return nil
 }
